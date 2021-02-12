db951442ba7790a94341224571abc4bdebd597c1
Berat Bora <gokhanarzub@gmail.com>
2021-02-12 21:52:26 +0300

    added board gen, board formatting and note gen

--- main.py
+++ main.py
@@ -4,77 +4,102 @@ from pprint import pprint
 
 
 class Difficulty:
-	easy = 1
-	medium = 2
-	hard = 3
+    easy = 1
+    medium = 2
+    hard = 3
 
 class Game:
-	def __init__(self, difficulty):
-		self.difficulty = difficulty
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
 
-		self.points = 0
-		self.lineno = 0
-		self.duration = 20
-		self.strings = 4
+        self.points = 0
+        self.lineno = 0
+        self.duration = 20
+        self.strings = 4
+        self.num_lines_visible = 10
 
-		self.board = self.generate_board()
+        self.board = self.generate_board()
 
-	def generate_board(self):
-		# generates upcoming notes, as 2D list
+    def generate_board(self):
+        # generates upcoming notes, as 2D list
 
-		board = [self.generate_notes() for _ in range(self.duration)]
-		return board
-	
-	def generate_notes(self):
-		# generates a single row of board
-		options = (0,)*3 + (1,)*1 + (2,)*1
-		note_count = choice(options) 
-		blank_count = self.strings - note_count 
-				
-		notes = [False]*blank_count + [True]*note_count
+        board = [self.generate_notes() for _ in range(self.duration)]
+        return board
+    
+    def generate_notes(self):
+        # generates a single row of board
+        options = (0,)*3 + (1,)*1 + (2,)*1
+        note_count = choice(options) 
+        blank_count = self.strings - note_count 
+                
+        notes = [False]*blank_count + [True]*note_count
 
-		shuffle(notes)
-		return notes
-				
-				
-
-
+        shuffle(notes)
+        return notes
+                
+                
 
 
-	def get_keys(self):
-		# user input
-		...
 
-	def format_board(self):
-		# formats just incoming notes (as string)
-		...
 
-	def format(self):
-		# formats game state as string
-		...
+    def get_keys(self):
+        # user input
+        ...
 
-	def display_score(self):
-		# formats a screen that displays score
-		...
+    def format_line(self, line):
+        formatted = []
+        for note in line:
+            symbol = "X" if note else " "
+            formatted.append(symbol)
 
-	def play(self):
-		# play the game
-		...
+        return " ".join(formatted)
+
+        
+
+
+    def format_board(self):
+        # formats just incoming notes (as string)
+        #TODO print only next n lines, pad with empty lines if neccessary
+        visible_lines = self.board[self.lineno:self.lineno + self.num_lines_visible]
+        return "\n".join(map(self.format_line, visible_lines))
+        
+    
+    def format(self):
+        # formats game state as string
+        formatted_keys = " ".join("ASDF") 
+        formatted_hyps = "-" * 7
+        formatted_board = self.format_board()
+
+        result = f"{formatted_board}\n{formatted_hyps}\n{formatted_keys}"
+
+        return result
+
+    def display_score(self):
+        # formats a screen that displays score
+        ...
+
+    def play(self):
+        # play the game
+        ...
 
 def main():
-	
-	#stdscr.clear()
-	
-	curr_game = Game(Difficulty.easy)
-	curr_game.play()
-	pprint(curr_game.board) 
-	
+    
+    #stdscr.clear()
+    
+    curr_game = Game(Difficulty.easy)
+    curr_game.play()
+    #pprint(curr_game.board) 
+    print(curr_game.format())
+    
 
 
 
 
-	input()
+    input()
 
 
 #curses.wrapper(main)
 main() 
+
+
+
